d6a0cc1656c53fc29dded40f2a52f733e2320f38
kosuke YOSHIMOTO <kosuke.yoshimoto@gmail.com>
2024-06-03 13:29:33 +0900

    Add config.

---
+++ src/config.rs
@@ -0,0 +1,11 @@
+pub const TEMPLATE: &str = "\
+use proconio::*;
+
+fn main() {
+    input! {
+    }
+}
+";
+
+pub const TESTCASE_DIR_NAME: &str = "testcase";
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod config;
 mod cmd;
 mod syscommand;
 
